de1416bc1e94c009160312d6f8e9bd9c7f75db50
Pratikshametri51 <Pratikshametri50@gmail.com>
2026-03-13 12:19:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import streamlit as st
 import pickle
 import numpy as np
 
-# Load Model and Scaler
+# Load model and scaler
 model = pickle.load(open("model.pkl","rb"))
 scaler = pickle.load(open("scaler.pkl","rb"))
 
 st.title("Credit Card Default Prediction")
 
-st.write("Enter customer details:")
+st.write("Enter Customer Details")
 
 LIMIT_BAL = st.number_input("Credit Limit")
 SEX = st.selectbox("Sex", [1,2])
-EDUCATION = st.selectbox("Education",[1,2,3,4])
-MARRIAGE = st.selectbox("Marriage",[1,2,3])
+EDUCATION = st.selectbox("Education", [1,2,3,4])
+MARRIAGE = st.selectbox("Marriage", [1,2,3])
 AGE = st.number_input("Age")
 
 PAY_0 = st.number_input("PAY_0")
@@ -39,18 +39,18 @@ PAY_AMT6 = st.number_input("PAY_AMT6")
 
 if st.button("Predict"):
 
-    input_data = np.array([[LIMIT_BAL, SEX, EDUCATION, MARRIAGE, AGE,
-                            PAY_0, PAY_2, PAY_3, PAY_4, PAY_5, PAY_6,
-                            BILL_AMT1, BILL_AMT2, BILL_AMT3,
-                            BILL_AMT4, BILL_AMT5, BILL_AMT6,
-                            PAY_AMT1, PAY_AMT2, PAY_AMT3,
-                            PAY_AMT4, PAY_AMT5, PAY_AMT6]])
+    data = np.array([[LIMIT_BAL, SEX, EDUCATION, MARRIAGE, AGE,
+                      PAY_0, PAY_2, PAY_3, PAY_4, PAY_5, PAY_6,
+                      BILL_AMT1, BILL_AMT2, BILL_AMT3,
+                      BILL_AMT4, BILL_AMT5, BILL_AMT6,
+                      PAY_AMT1, PAY_AMT2, PAY_AMT3,
+                      PAY_AMT4, PAY_AMT5, PAY_AMT6]])
 
-    input_scaled = scaler.transform(input_data)
+    data = scaler.transform(data)
 
-    prediction = model.predict(input_scaled)
+    prediction = model.predict(data)
 
     if prediction[0] == 1:
-        st.error("Customer may DEFAULT payment")
+        st.error("Customer will DEFAULT payment")
     else:
         st.success("Customer will NOT default")
